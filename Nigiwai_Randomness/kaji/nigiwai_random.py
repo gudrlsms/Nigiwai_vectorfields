4dcaa14e7d0a11287046d2ef6e2771f30572eeaf
shizuo-kaji <shizuo.kaji@gmail.com>
2021-11-01 14:07:33 +0900

    parallel

--- Nigiwai_Randomness/kaji/nigiwai_random.py
+++ Nigiwai_Randomness/kaji/nigiwai_random.py
@@ -2,16 +2,26 @@ import matplotlib.pyplot as plt
 import scipy.stats
 import numpy as np
 from tqdm import tqdm
+from numba import njit, prange
+import pandas as pd
+
+def load_vadere(fn):
+    df = pd.read_csv(fn,header=0,delim_whitespace=True,dtype='f8')
+    df['pedestrianId'] -= df['pedestrianId'].min() # PID start with 0
+    return(df[['pedestrianId','simTime','endTime-PID1','startX-PID1','endX-PID1','startY-PID1','endY-PID1']].values)
 
 # Loading vadere data
-def load_vadere(df, start_frame=0, end_frame=None, frame_skip=1):
+@njit(parallel=True)
+def vadere_coords(A, start_frame=0, end_frame=None, frame_skip=1):
+    # A['pedestrianId','simTime','endTime-PID1','startX-PID1','endX-PID1','startY-PID1','endY-PID1']
     if end_frame is None:
-        end_frame = int(round(df['endTime-PID1'].max()/frame_skip))
-    nPed = int(df['pedestrianId'].max())
-    df['pedestrianId'] -= df['pedestrianId'].min() # PID start with 0
-    X = np.full((nPed,end_frame),np.nan)  # (pid,frame),  x == nan means the person is not in the field
-    Y = np.full((nPed,end_frame),np.nan)  # (pid,frame)
-    for pid,st,et,sx,ex,sy,ey in tqdm(zip(df['pedestrianId'],df['simTime'],df['endTime-PID1'],df['startX-PID1'],df['endX-PID1'],df['startY-PID1'],df['endY-PID1']),total=df.shape[0]):
+        end_frame = int(round(A[:,2].max()/frame_skip))
+    nPed = int(A[:,0].max())
+    X = np.full((nPed,int(end_frame)),np.nan)  # (pid,frame),  x == nan means the person is not in the field
+    Y = np.full((nPed,int(end_frame)),np.nan)  # (pid,frame)
+    #for pid,st,et,sx,ex,sy,ey in tqdm(zip(df['pedestrianId'],df['simTime'],df['endTime-PID1'],df['startX-PID1'],df['endX-PID1'],df['startY-PID1'],df['endY-PID1']),total=df.shape[0]):
+    for i in range(len(A)):
+        pid,st,et,sx,ex,sy,ey = A[i]
         pid = int(pid)
         start_fr = max(int(round(st/frame_skip)),start_frame)
         end_fr = min(int(round(et/frame_skip))+1,end_frame)
@@ -61,11 +71,16 @@ def entropy_base(u,v):
     return(np.maximum(0,1-ent/np.log(u.shape[1])))
     #return(np.where(np.isnan(ent), 0, 1-ent/np.log(u.shape[1])))
     
-def moving_average(values, alpha=0.8):
-    indicator_list_ma = []
-    indicator_list_ma.append(values[0])
-    for i in range(len(values)-1):
-        indicator_list_ma.append(alpha*values[i+1] + (1-alpha)*indicator_list_ma[i])
+def moving_average(values, alpha=0.8, window=5, method='rolling'):
+    if method=='rolling':
+        indicator_list_ma = pd.DataFrame(values).rolling(window=window).mean()
+    elif method=='ewm':
+        indicator_list_ma = pd.DataFrame(values).ewm(alpha=alpha).mean()
+    else:
+        indicator_list_ma = []
+        indicator_list_ma.append(values[0])
+        for i in range(len(values)-1):
+            indicator_list_ma.append(alpha*values[i+1] + (1-alpha)*indicator_list_ma[i])
     return(indicator_list_ma)
 
 # not used
